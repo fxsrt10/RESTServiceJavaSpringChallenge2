34d2779123c9312f1240c3c3e6778f73eaf9ad84
Ben <b@test.com>
2017-07-27 14:40:04 +0000

    adding codesss

--- src/main/java/hello/MySQLConnection.java
+++ src/main/java/hello/MySQLConnection.java
@@ -9,17 +9,22 @@ import java.util.List;
  * @author bpeters
  */
 public class MySQLConnection {
-	private Connection conn;
-	private Statement stmt;
+	private static Connection conn;
+	private static Statement stmt;
 
 	public MySQLConnection() {
 		try {
 			System.out.println("----connecting to mysql----");
 			Class.forName("com.mysql.jdbc.Driver");
+			if(conn != null){
 			conn = DriverManager.getConnection("jdbc:mysql://129.150.74.214:3306/mydatabase", "root", "Oracle1!");
+			
 			// here sonoo is database name, root is username and password
 			System.out.println("connection established");
+			
 			stmt = conn.createStatement();
+			}
+			
 		} catch (Exception e) {
 
 		}
@@ -30,7 +35,7 @@ public class MySQLConnection {
 		try {
 
 			System.out.println("test");
-			ResultSet rs = stmt.executeQuery("SELECT Original_Mortgage_Amount FROM refinance WHERE ID =" + id);
+			ResultSet rs = stmt.executeQuery("SELECT Original_Mortgage_Amount FROM purchase WHERE ID =" + id);
 			while (rs.next())
 				mortgageAmount = Double.valueOf(rs.getString(1).replace(",", ""));
 			System.out.println(mortgageAmount);
@@ -64,7 +69,48 @@ public class MySQLConnection {
 		try {
 
 			System.out.println("test");
-			ResultSet rs = stmt.executeQuery("SELECT * FROM refinance WHERE Property_City = " +"'"+ city +"'");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM purchase WHERE Property_City = " +"'"+ city +"'");
+			while (rs.next()){
+				System.out.println("---------");
+				System.out.println(rs.getInt(1));
+				Purchase p = new Purchase();
+				p.id = rs.getInt(1);
+				p.setPropertyState(rs.getString(2));
+				p.setPropertyCounty(rs.getString(4));
+				p.setPropertyCity(rs.getString(3));
+				p.setPropertyZip(rs.getString(5));
+				p.setSponsorOriginator(rs.getString(6));
+				p.setMortgageNumber(rs.getString(7));
+				p.setSponsorName(rs.getString(8));
+				p.setSponsorNumber(rs.getString(9));
+				p.setDownpaymentSource(rs.getString(10));
+				p.setNonProfitNumber(rs.getString(11));
+				p.setProductType(rs.getString(12));
+				p.setLoanPurpose(rs.getString(13));
+				p.setPropertyType(rs.getString(14));
+				p.setInterestRate(rs.getFloat(15));
+				p.setOrigMortgageAmount(rs.getDouble(16));
+			
+				
+				purchases.add(p);
+			}
+			
+				
+			
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return purchases;
+	}
+	public List<Purchase> getPurchases() {
+		List<Purchase> purchases = new ArrayList<>();
+
+		try {
+
+			System.out.println("test");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM purchase");
 			while (rs.next()){
 				System.out.println("---------");
 				System.out.println(rs.getInt(1));
--- src/main/java/hello/PurchaseController.java
+++ src/main/java/hello/PurchaseController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,17 @@ public class PurchaseController {
     private final AtomicLong counter = new AtomicLong();
     public MySQLConnection db = new MySQLConnection();
 
+    @RequestMapping(method = RequestMethod.GET)
+    @Async
+    public List<Purchase> getPurchases(){
+    	List<Purchase> purchases = new ArrayList<>();
+    	purchases = db.getPurchases();
+    	
+    	
+    	
+    	
+    	return purchases;    	
+    }
     @RequestMapping(method = RequestMethod.GET, value = "{id}")
     public Purchase getById(@PathVariable String id) {
         Double amount = db.getMortgageAmount(id);
