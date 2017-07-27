490f06ad7f38eb312aa3e1197ce8637af0dd2d1b
Ben <b@test.com>
2017-07-27 04:25:23 +0000

    adding code8

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,33 @@
 package hello;
 
+import java.util.Optional;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	// Get PORT and HOST from Environment or set default
+    public static final Optional <String> host;
+    public static final Optional <String>port;
+    public static final Properties myProps = new Properties();
+
+    static {
+        host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        // Set properties
+
+        myProps.setProperty("server.address", host.orElse("localhost"));
+        myProps.setProperty("server.port", port.orElse("8080"));
+
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setDefaultProperties(myProps);
+        app.run(args);
+
     }
+    
 }
